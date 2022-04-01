f269f7000631a5a8adcbfcf2d083e982a52941cc
Mihail Filipescu <Mihail.Filipescu@endava.com>
2022-04-01 11:34:20 +0300

    extracted httpClient as a variable

--- app/src/main/java/com/endava/internship/mobile/weatherapp/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/endava/internship/mobile/weatherapp/data/remote/RemoteDataSource.kt
@@ -7,26 +7,27 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object RemoteDataSource {
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor {chain ->
+            chain.proceed(chain.request().newBuilder()
+                .url(chain.request().url.newBuilder()
+                    .addQueryParameter(Constants.WEATHER_API_QUERY_APP_ID, Constants.WEATHER_API_KEY)
+                    .build()
+                )
+                .build())
+        }.also { client ->
+            if (BuildConfig.DEBUG) {
+                val logging = HttpLoggingInterceptor()
+                logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+                client.addInterceptor(logging)
+            }
+        }.build()
+
     fun <Api> buildApi(api: Class<Api>): Api {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL_RETROFIT_API)
-            .client(
-                OkHttpClient.Builder()
-                    .addInterceptor {chain ->
-                        chain.proceed(chain.request().newBuilder()
-                            .url(chain.request().url.newBuilder()
-                                .addQueryParameter(Constants.WEATHER_API_QUERY_APP_ID, Constants.WEATHER_API_KEY)
-                                .build()
-                            )
-                            .build())
-                    }.also { client ->
-                        if (BuildConfig.DEBUG) {
-                            val logging = HttpLoggingInterceptor()
-                            logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
-                            client.addInterceptor(logging)
-                        }
-                    }.build()
-            )
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(api)
