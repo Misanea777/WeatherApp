148dd4aa3db04d4747f04d725ff279b890d437e1
Mihail Filipescu <Mihail.Filipescu@endava.com>
2022-04-12 19:22:25 +0300

    made safeApi top-lvl func & added dsipatchers injection & extracted repo func  into interface

--- app/src/main/java/com/endava/internship/mobile/weatherapp/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/endava/internship/mobile/weatherapp/data/repository/WeatherRepository.kt
@@ -1,45 +1,30 @@
 package com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.data.repository
 
-import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.utils.ExcludeList
-import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.utils.LatLong
+import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.di.dispatchers.IoDispatcher
 import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.network.Resource
-import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.network.SafeApiCall
-import com.endava.internship.mobile.weatherapp.data.model.forecast.Daily
+import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.network.safeApiCall
+import com.endava.internship.mobile.weatherapp.com.endava.internship.mobile.weatherapp.utils.LatLong
 import com.endava.internship.mobile.weatherapp.data.model.forecast.ForecastResponse
 import com.endava.internship.mobile.weatherapp.data.remote.WeatherApi
-import com.endava.internship.mobile.weatherapp.utils.Constants
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class WeatherRepository @Inject constructor(
-    private val api: WeatherApi
-) : SafeApiCall {
-    suspend fun getDailyForecast(
-        latLong: LatLong,
-        days: Int = Constants.MAX_DAILY_FORECAST_DAYS
-    ): Resource<List<Daily>> = safeApiCall {
-        api.getWeatherDataFromLatLong(
-            lat = latLong.lat, long = latLong.long,
-            excludeFields = ExcludeList(
-                listOf(
-                    Constants.WEATHER_API_QUERY_FIELD_MINUTELY,
-                    Constants.WEATHER_API_QUERY_FIELD_ALERTS,
-                    Constants.WEATHER_API_QUERY_FIELD_CURRENT,
-                    Constants.WEATHER_API_QUERY_FIELD_HOURLY
-                )
-            ),
-        ).daily!!.take(days + 1).drop(1)
-    }
+    private val api: WeatherApi,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+)  {
+    suspend fun getDailyForecastFromLatLong(latLong: LatLong): Resource<ForecastResponse> =
+        safeApiCall(apiCall = {
+            api.getDailyForecastFromLatLong(
+                lat = latLong.lat,
+                long = latLong.long
+            )
+        }, dispatcher = ioDispatcher)
 
-    suspend fun getHourlyForecast(latLong: LatLong): Resource<ForecastResponse> = safeApiCall {
-        api.getWeatherDataFromLatLong(
-            lat = latLong.lat, long = latLong.long,
-            excludeFields = ExcludeList(
-                listOf(
-                    Constants.WEATHER_API_QUERY_FIELD_MINUTELY,
-                    Constants.WEATHER_API_QUERY_FIELD_ALERTS,
-                    Constants.WEATHER_API_QUERY_FIELD_DAILY
-                )
-            ),
-        )
-    }
+    suspend fun getHourlyForecastFromLatLong(latLong: LatLong): Resource<ForecastResponse> =
+        safeApiCall(apiCall = {
+            api.getHourlyForecastFromLatLong(lat = latLong.lat, long = latLong.long)
+        }, dispatcher = ioDispatcher)
 }
